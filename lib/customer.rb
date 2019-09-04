0869d16212f4a29a2d13745de0d49a33ba2797ba
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-04 18:41:30 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,18 @@
 class Customer
+  attr_accessor :name, :age 
+  @@all = []
   
+  def initialize(name,age)
+    @name = name 
+    @age = age 
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  def new_meal(waiter,total,tip=0)
+  Meal.new(waiter,self,total,tip)
+end
+#don't need to take in customer because you can just use self as the current customers instance.
 end
