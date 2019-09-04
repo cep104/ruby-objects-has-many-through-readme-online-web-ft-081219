a947076a68154f4d3651456c5fdd6128ca6902ea
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-04 19:08:47 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -15,9 +15,16 @@ end
     Meal.new(self, customer, total, tip)
   end
   
-  def waiters
-  meals.map do |meal|
-    meal.waiter 
+  def meals 
+    Meal.all.select do |meal|
+      meal.waiter == self
+    end
   end
-end
+  
+  def best_tipper 
+   best_tipped_meal = meals.max do |meal_a, meal_b|
+     meal_a.tip <=> meal_b.tip 
+   end
+   best_tipped_meal.customer
+  end
 end
